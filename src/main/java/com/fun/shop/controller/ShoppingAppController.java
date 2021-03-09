5e508dc588d24a41d9da53f16fee6f076d460c49
Ankeet Maini <ankeet.maini@gmail.com>
2021-03-09 22:06:30 +0530

    add everything in one file to make it work

--- src/main/java/com/fun/shop/controller/ShoppingAppController.java
+++ src/main/java/com/fun/shop/controller/ShoppingAppController.java
@@ -1,14 +1,16 @@
 package com.fun.shop.controller;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.fun.shop.domain.Product;
-import com.fun.shop.repository.ProductRepository;
+import javax.annotation.PostConstruct;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,19 +22,24 @@ public class ShoppingAppController {
 	}
 }
 
+@RestController
+class ConfigParamsController {
+  @Value("${environment}")
+  private String environment;
+
+  @GetMapping("/environment")
+  String getEnvironment() {
+    return environment;
+  }
+}
+
 
 @RestController
 class ProductService {
 	private ProductRepository productRepository;
 	
-	public ProductService(ProductRepository productRepository) {
-		productRepository.saveAll(List.of(
-			new Product("1", "potato"),
-			new Product("2", "onions"),
-			new Product("3", "chips"),
-			new Product("4", "fruits")
-		))
-		;
+	public ProductService(ProductRepository repository) {
+		this.productRepository = repository;
 	}
 
 	@GetMapping(value = "/products")
@@ -44,4 +51,56 @@ class ProductService {
 	Optional<Product> getProductById(@PathVariable String id) {
 		return productRepository.findById(id);
 	}
+}
+
+
+@Entity
+class Product {
+  
+  @Id
+	private String id;
+	private String name;
+	
+	public Product(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+  public Product() {}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+interface ProductRepository extends CrudRepository<Product, String>{
+}
+
+
+@Component
+class DataDump {
+  private ProductRepository productRepository;
+
+  public DataDump(ProductRepository repository) {
+    this.productRepository = repository;
+  }
+
+  @PostConstruct
+  public void dump() {
+    productRepository.saveAll(List.of(
+			new Product("1", "potato"),
+			new Product("2", "onions"),
+			new Product("3", "chips"),
+			new Product("4", "fruits")
+		));
+  }
 }
--- src/main/java/com/fun/shop/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fun.shop.repository;
-
-import com.fun.shop.domain.Product;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, String>{
-  
-}
